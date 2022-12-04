c60597d57d4b9c1f012ef7a0ed6a9b2b5ee16195
Fernando Ayats <ayatsfer@gmail.com>
2022-12-04 12:58:38 +0100

    day 4 part 1

--- advent4/src/main.rs
+++ advent4/src/main.rs
@@ -1,11 +1,43 @@
+use log::{debug, info};
 use std::error::Error;
-use log::debug;
 
 fn main() -> Result<(), Box<dyn Error>> {
     advent_common::setup_logging()?;
     let file = advent_common::file()?;
+    // let file = std::fs::read_to_string("input.txt")?;
 
-    debug!("{}", file);
+    let contained = file
+        .lines()
+        .map(|line| {
+            debug!("\n>{}<", line);
+
+            let elements = line
+                .split(&[',', '-'][..])
+                .map(|e| e.parse().unwrap())
+                .collect::<Vec<u32>>();
+
+            debug!("{:?}", elements);
+
+            let (left, right) = if let [a, b, c, d] = *elements.as_slice() {
+                ((a, b), (c, d))
+            } else {
+                panic!("Didn't find 4 elements")
+            };
+
+            let right_contained = right.1 <= left.1 && right.0 >= left.0;
+            let left_contained = left.1 <= right.1 && left.0 >= right.0;
+
+            let result = right_contained | left_contained;
+            debug!("{}", result);
+            result
+        })
+        .collect::<Vec<_>>();
+
+    let part1_result: u64 = contained
+        .iter()
+        .fold(0, |acc, &c| acc + if c { 1 } else { 0 });
+
+    info!("part1 result: {}", part1_result);
 
     Ok(())
 }
