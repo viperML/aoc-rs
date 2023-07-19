d8e6d9402578347b1e5d223eb901a484c45b0c46
Fernando Ayats <ayatsfer@gmail.com>
2023-07-19 20:25:36 +0200

    advent8 testing

--- advent8/src/main.rs
+++ advent8/src/main.rs
@@ -2,10 +2,47 @@ use advent_common::*;
 use eyre::Result;
 use tracing::info;
 
-fn main() -> Result<()> {
-    setup_logging()?;
+#[derive(Debug, Default)]
+struct Tree<'neighbour> {
+    height: u64,
 
-    info!("hllo");
+    up: Vec<&'neighbour Tree<'neighbour>>,
+    down: Vec<&'neighbour Tree<'neighbour>>,
+    left: Vec<&'neighbour Tree<'neighbour>>,
+    right: Vec<&'neighbour Tree<'neighbour>>,
+}
+
+fn main() -> Result<()> {
+    advent_common::setup_logging()?;
+    let input = advent_common::file()?;
+
+    let size = input.lines().count() as i32;
+    info!(?size);
+
+    let trees = input
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|t| t.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+
+    eyre::ensure!(size * size == trees.len() as _, "Size was incorrect");
+
+    let mut visibles = 0;
+
+    let res = trees.iter().enumerate().map(|(idx, t)| {
+        if (1 - size) <= 0 || size - 1 >= size * size {
+            return true;
+        }
+        let range = (1 - size as usize)..(size as usize - 1);
+
+        match trees.get(range) {
+            None => true,
+            Some(range) => range.iter().all(|elem| elem < t),
+        }
+    }).collect::<Vec<_>>();
+
+    info!(?trees);
+    info!(?res);
 
     Ok(())
 }
